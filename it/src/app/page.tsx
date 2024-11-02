e62387e955081ffab0b936fd0b7cac1805a28239
Salman-Ghafoor <salmanqc26@gmail.com>
2024-11-02 22:11:46 +0500

    initial commit

---
+++ it/src/app/page.tsx
@@ -0,0 +1,21 @@
+export default function Home() {
+  return (
+    <div >
+    <main className="flex  justify-between  gap-2 ">
+      <div className="bg-black w-6/12 h-80  align-middle shadow-2xl  "> </div>
+      <div className="bg-blue-950 w-6/12 h-80">
+    </div>
+    </main>
+    <br />
+
+        <div className="flex flex-row h-screen gap-2">
+          <div className="bg-red-950 w-4/12 h-60 "></div>
+          <div className="bg-neutral-950 w-4/12 h-60"></div>
+          <div className="bg-stone-950 w-4/12 h-60"></div>
+
+        </div>
+
+    
+    </div>
+  );
+}
